f1028ee37077f4d03b6db48a04a969e574584649
sulikz <lsuliborski@gmail.com>
2023-04-03 20:41:19 +0200

    Fixed var issue

--- file-layout-example/stage/services/webserver-cluster/main.tf
+++ file-layout-example/stage/services/webserver-cluster/main.tf
@@ -17,24 +17,18 @@ resource "aws_launch_configuration" "example" {
   image_id        = "ami-0fb653ca2d3203ac1"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
-  user_data = data.template_file.user_data.rendered
+  user_data = templatefile("user-data.sh", {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  })
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
-data "template_file" "user_data" {
-  template = file("user-data.sh")
-
-  vars = {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
-  }
-}
-
 resource "aws_security_group" "instance" {
-
   name = var.security_group_name
 
   ingress {
--- file-layout-example/stage/services/webserver-cluster/user-data.sh
+++ file-layout-example/stage/services/webserver-cluster/user-data.sh
@@ -4,5 +4,5 @@ cat > index.html << EOF
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
 EOF
-nohup busybox httpd -f -p ${var.server_port} &
+nohup busybox httpd -f -p ${server_port} &
 
