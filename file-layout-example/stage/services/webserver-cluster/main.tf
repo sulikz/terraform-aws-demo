e798a2d575d0e3dc7e202df673ec3320d057bf94
sulikz <lsuliborski@gmail.com>
2023-04-03 20:16:37 +0200

    bash added to .sh file

--- file-layout-example/stage/services/webserver-cluster/main.tf
+++ file-layout-example/stage/services/webserver-cluster/main.tf
@@ -17,18 +17,19 @@ resource "aws_launch_configuration" "example" {
   image_id        = "ami-0fb653ca2d3203ac1"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello World" >> index.html
-              echo "test" >> index.html 
-              echo "${data.terraform_remote_state.db.outputs.address}" >> index.html
-              echo "${data.terraform_remote_state.db.outputs.port}" >> index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
+  user_data = data.template_file.user_data.rendered
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+data "template_file" "user_data" {
+  template = file("user-data.sh")
+
+  vars = {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
   }
 }
 
@@ -155,3 +156,4 @@ data "terraform_remote_state" "db" {
     region = "us-east-2"
   }
 }
+
